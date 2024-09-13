bd6f9be5b01ff07ab67c46d3c9b110cac9f6eda8
Aasmund Nørsett <aasmund.norsett@gmail.com>
2024-09-13 14:38:44 +0200

    docs(timer): add function descriptions

--- atmega162/include/timer.h
+++ atmega162/include/timer.h
@@ -1,8 +1,15 @@
 #ifndef __TIMER__
 #define __TIMER__
 
+/**
+ * Initialize hardware timers
+ */
 void TIMER_init();
 
+/**
+ * Set TIMER1 COMPA interrupt callback
+ * @param cb pointer to new callback
+ */
 void TIMER_set_TIMER1_COMPA_cb(void (*cb)());
 
 #endif // __TIMER__
