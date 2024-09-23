0efe0b0d7d9c6c73a0f5db50efaa013b5ad6fe71
William Skaftnes <willisk@stud.ntnu.no>
2024-09-23 19:04:59 +0200

    feat: initial oled driver setup

--- atmega162/src/main.c
+++ atmega162/src/main.c
@@ -10,6 +10,7 @@
 #include "sys.h"
 #include "timer.h"
 #include "uart.h"
+#include "oled.h"
 
 #define NUM_PINS 1
 
@@ -52,18 +53,30 @@ int main()
   TIMER_init();
   TIMER_set_TIMER1_COMPA_cb(heartbeat_cb);
 
+  OLED_init();
+  OLED_reset(); 
+
+  OLED_set_pos(0,0);
+  OLED_write_char('s');
+  OLED_write_char('u');
+  OLED_write_char('S');
+  OLED_print("Sus");
+
+  OLED_set_pos(4, 30);
+  
   ADC_calibrate_joystick();
 
   printf("\r\nSetup complete\r\n");
 
   while (1)
   {
-    joystick_pos_t pos = ADC_get_joystick_pos();
-    printf("\033[2J\033[2;0H\r  ");
-    printf("dir: %d slider1: %d slider2: %d \r\n", ADC_calc_joystick_dir(pos),
-           ADC_get_slider_pos(SLIDER_LEFT_INDEX),
-           ADC_get_slider_pos(SLIDER_RIGHT_INDEX));
 
-    _delay_ms(20);
+       // joystick_pos_t pos = ADC_get_joystick_pos();
+    // printf("\033[2J\033[2;0H\r  ");
+    // printf("dir: %d slider1: %d slider2: %d \r\n", ADC_calc_joystick_dir(pos),
+    //        ADC_get_slider_pos(SLIDER_LEFT_INDEX),
+    //        ADC_get_slider_pos(SLIDER_RIGHT_INDEX));
+
+    // _delay_ms(20);
   }
 }
