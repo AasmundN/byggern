4ce3931d11d0a2c094a17291f7b82e4497f54f72
Aasmund Nørsett <aasmund.norsett@gmail.com>
2024-10-07 13:08:35 +0200

    feat: driver for can controller spi interface

--- atmega162/include/can.h
+++ atmega162/include/can.h
@@ -1,10 +1,22 @@
 #ifndef __CAN__
 #define __CAN__
 
+#define T0 1
+#define T1 2
+#define T2 4
+
 void CAN_init();
 
 void CAN_controller_read(char addr, char data_buffer[], int buffer_size);
 
 void CAN_controller_write(char addr, char data_buffer[], int buffer_size);
+
+void CAN_controller_rts(int buffer_select_bits);
+
+char CAN_controller_read_status();
+
+void CAN_controller_bit_mod(char addr, char mask, char data);
+
+void CAN_controller_reset();
 
 #endif // __CAN__
--- atmega162/src/can.c
+++ atmega162/src/can.c
@@ -6,21 +6,27 @@
 // controller command IDs
 #define WRITE_CMD 0x2
 #define READ_CMD 0x3
+#define RTS_CMD_BASE 0x80
+#define READ_STATUS_CMD 0xa0
+#define BIT_MOD_CMD 0x5
+#define RESET_CMD 0xc0
 
-void CAN_init() { SPI_init(); }
+void CAN_init()
+{
+  SPI_init();
+  CAN_controller_reset();
+}
 
 void CAN_controller_read(char addr, char data_buffer[], int buffer_size)
 {
   SPI_slave_select(SELECT, SS_PIN);
 
-  SPI_write_byte(READ_CMD);
+  SPI_shift_data(READ_CMD);
 
-  SPI_write_byte(addr);
+  SPI_shift_data(addr);
 
   for (int i = 0; i < buffer_size; i++)
-  {
-    data_buffer[i] = SPI_read_byte();
-  }
+    data_buffer[i] = SPI_shift_data(DONT_CARE);
 
   SPI_slave_select(DESELECT, SS_PIN);
 }
@@ -29,12 +35,58 @@ void CAN_controller_write(char addr, char data_buffer[], int buffer_size)
 {
   SPI_slave_select(SELECT, SS_PIN);
 
-  SPI_write_byte(WRITE_CMD);
+  SPI_shift_data(WRITE_CMD);
 
-  SPI_write_byte(addr);
+  SPI_shift_data(addr);
 
   for (int i = 0; i < buffer_size; i++)
-    SPI_write_byte(data_buffer[i]);
+    SPI_shift_data(data_buffer[i]);
+
+  SPI_slave_select(DESELECT, SS_PIN);
+}
+
+void CAN_controller_rts(int buffer_select_bits)
+{
+  SPI_slave_select(SELECT, SS_PIN);
+
+  SPI_shift_data(RTS_CMD_BASE | buffer_select_bits);
+
+  SPI_slave_select(DESELECT, SS_PIN);
+}
+
+char CAN_controller_read_status()
+{
+  SPI_slave_select(SELECT, SS_PIN);
+
+  SPI_shift_data(READ_STATUS_CMD);
+
+  char status = SPI_shift_data(DONT_CARE);
+
+  SPI_slave_select(DESELECT, SS_PIN);
+
+  return status;
+}
+
+void CAN_controller_bit_mod(char addr, char mask, char data)
+{
+  SPI_slave_select(SELECT, SS_PIN);
+
+  SPI_shift_data(BIT_MOD_CMD);
+
+  SPI_shift_data(addr);
+
+  SPI_shift_data(mask);
+
+  SPI_shift_data(data);
+
+  SPI_slave_select(DESELECT, SS_PIN);
+}
+
+void CAN_controller_reset()
+{
+  SPI_slave_select(SELECT, SS_PIN);
+
+  SPI_shift_data(RESET_CMD);
 
   SPI_slave_select(DESELECT, SS_PIN);
 }
