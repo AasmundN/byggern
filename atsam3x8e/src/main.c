01052047de9762efc1b62f54f825688966a58feb
willisk0612 <willisk@stud.ntnu.no>
2024-11-07 17:56:18 +0100

    feat:gpio driver

--- atsam3x8e/src/main.c
+++ atsam3x8e/src/main.c
@@ -1,8 +1,10 @@
+#include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
 
 #include "adc.h"
 #include "can.h"
+#include "gpio.h"
 #include "sam.h"
 #include "servo.h"
 #include "tc.h"
@@ -10,6 +12,14 @@
 
 #define F_CPU 84000000
 #define BAUDRATE 9600
+
+#define NUM_PINS 2
+#define SOLENOID_PIN PIOB, PIO_PB19
+#define MOTOR_DIR_PIN PIOC, PIO_PC23
+
+pin_config_t pin_configs[] = {
+    {SOLENOID_PIN, .direction = OUTPUT, .pullup = false},
+    {MOTOR_DIR_PIN, .direction = OUTPUT, .pullup = false}};
 
 CanInit_t bit_timing = {
     .phase2 = 3,
@@ -22,14 +32,12 @@ CanInit_t bit_timing = {
 
 CanMsg receive_can;
 
-typedef struct
-{
+typedef struct {
   int8_t x;
   int8_t y;
 } joystick_pos_t;
 
-typedef enum
-{
+typedef enum {
   LEFT,
   RIGHT,
   UP,
@@ -37,10 +45,8 @@ typedef enum
   NEUTRAL,
 } joystick_dir_t;
 
-union
-{
-  struct
-  {
+union {
+  struct {
     // joystick
     joystick_dir_t joystick_dir : 8;
     joystick_pos_t joystick_pos;
@@ -53,10 +59,11 @@ union
   Byte8 buffer;
 } input_data;
 
+uint8_t prev_btn_state = 1;
+unsigned int btn_on_count = 0;
 int8_t servo_pos = 0;
 
-int main()
-{
+int main() {
   SystemInit();
 
   WDT->WDT_MR = WDT_MR_WDDIS; // Disable Watchdog Timer
@@ -72,15 +79,16 @@ int main()
 
   ADC_init();
 
+  GPIO_init(pin_configs, NUM_PINS);
+  GPIO_write(SOLENOID_PIN, true);
+
   printf("Setup complete\r\n");
 
-  while (1)
-  {
+  while (1) {
     while (CAN_rx(&receive_can))
       ;
 
-    switch (receive_can.id)
-    {
+    switch (receive_can.id) {
     case INPUT_DATA_ID:
       memcpy(&input_data.buffer, &receive_can.byte8, sizeof(Byte8));
 
@@ -93,6 +101,18 @@ int main()
 
       servo_pos = input_data.joystick_pos.x;
       SERVO_set_pos(servo_pos);
+      if (prev_btn_state != input_data.joystick_btn_state) {
+        GPIO_write(SOLENOID_PIN, !!input_data.joystick_btn_state);
+      }
+      prev_btn_state = input_data.joystick_btn_state;
+      if (prev_btn_state)
+        btn_on_count = 0;
+      else
+        btn_on_count++;
+      if (btn_on_count >= 5) {
+        GPIO_write(SOLENOID_PIN, 1);
+      }
+      printf("%u\n\r", btn_on_count);
 
       break;
 
