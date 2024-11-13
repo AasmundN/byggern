8e10eeb2727de4766a5fffea528d9ca5f58bfbf0
Aasmund Nørsett <aasmund.norsett@gmail.com>
2024-11-13 11:39:32 +0100

    feat: change adc thresholds after redesigning ir circuit

--- atsam3x8e/src/adc.c
+++ atsam3x8e/src/adc.c
@@ -2,9 +2,9 @@
 #include "can.h"
 #include "sam.h"
 
-#define ADC_LOWTHRES 300
-#define ADC_HIGHTHRES 400
-#define MIN_REPEATED_UNDER_THRESHOLD 40
+#define ADC_LOWTHRES 200
+#define ADC_HIGHTHRES 700
+#define MIN_REPEATED_UNDER_THRESHOLD 30
 
 void ADC_init()
 {
@@ -46,7 +46,7 @@ void ADC_Handler(void)
         .length = 1,
     };
 
-    CAN_tx(goal_msg);
+    /*CAN_tx(goal_msg);*/
 
     should_end_game = false;
     repeated_under_thresh = 0;
